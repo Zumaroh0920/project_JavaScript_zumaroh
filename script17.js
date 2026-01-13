60f853aafdfc7a0a62c137b11cea1e908e2918d1
Zumaroh Hazanah Assmarqondi <zumarohazanah0920@gmail.com>
2026-01-13 10:13:12 +0700

    Add files via upload

---
+++ script17.js
@@ -0,0 +1,10 @@
+let nama = "otong";
+
+console.log("nama" + " = " + nama);
+
+if(nama==="ucup"){
+    console.log("alooo otong ");
+}else{
+    console.log("AUH AHH BUKANN otong")
+}
+console.log("Akhir dari Program");
